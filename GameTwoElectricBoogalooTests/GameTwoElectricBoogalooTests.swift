13e5761c913b02e96b349933ef44516f93672433
MonsterusCow <ryan.stark26@d155.org>
2024-10-10 15:13:27 -0500

    Initial Commit

---
+++ GameTwoElectricBoogalooTests/GameTwoElectricBoogalooTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameTwoElectricBoogalooTests.swift
+//  GameTwoElectricBoogalooTests
+//
+//  Created by RYAN STARK on 10/10/24.
+//
+
+import Testing
+@testable import GameTwoElectricBoogaloo
+
+struct GameTwoElectricBoogalooTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
